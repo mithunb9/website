eae6b81d95522e166d24bb0f91f3f226a23fdcf6
Mithun Balasubramanian <sendmithunb@gmail.com>
2024-08-22 19:43:11 -0500

    update file loading data

--- src/components/WorkExperience.vue
+++ src/components/WorkExperience.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="hover:bg-[#F5F9FD05] p-2 rounded-lg pb-4" @mouseover="$emit('mouseover', experience)">
-      <h2 class="text-txyellow text-xl font-bold">{{ experience.title }}</h2>
-      <p class="text-txwhite">{{ experience.company }} | {{ experience.date }}</p>
-      <p class="text-txwhite mt-2">{{ experience.description }}</p>
-    </div>
-  </template>
-  
-  <script setup>
-  defineProps({
-    experience: Object,
-  });
-  </script>
+  <div class="hover:bg-[#F5F9FD05] p-2 rounded-lg pb-4" @mouseover="$emit('mouseover', experience)">
+    <h2 class="text-txyellow text-xl font-bold">{{ experience.title }}</h2>
+    <p class="text-txwhite">{{ experience.company }} | {{ experience.date }}</p>
+    <p class="text-txwhite mt-2">{{ experience.description }}</p>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  experience: Object,
+});
+</script>
